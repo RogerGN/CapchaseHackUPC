d8f786279547ce61609a36dfe9a92f0cd67881b0
LordCatello <carmine_carratu@libero.it>
2022-04-30 15:48:14 +0200

    Code refactor and hopefully fixed the bug

--- main.rs
+++ main.rs
@@ -1,59 +1,24 @@
-/*
-for w in map.workers {
-    // Logic to check worker placement
-}
+// I assume that the map is (40 by 40)
+let WIDTH = 40;
+let HEIGHT = 40;
+let N_WORKERS = 8;
 
-for x in 0..40 {
-    for y in 0..40 {
-        if map[x][y] == Tile::EMPTY {
-            // more logic
-        }
-        // other logic
-    }
-}
-*/
 
-// 0: false
-// 1: true
-
-// 400 hundreds positions to allocate
-
-// I have to store already visited positions
-
-// improve bfs to choose not adjacent tiles.
-
-// worker that wants to 
-
-// use a worker to attack other players, just to create gaps in enemy territories, We could build circles. 
-
-// I assume map (40 by 40)
-let width = 40;
-let height = 40;
-
-let n_workers = 8;
-
-// I have to init collision matrix with positions of workers
-let collision_matrix = build_matrix(width, height);
-
-for w in 0..n_workers {
+// I have to init the collision matrix with the positions of the workers
+// The collision matrix keeps track of the actual or future positions of our workers
+// and is used in order to avoid to move a worker in a tile that is or will be occipied by another worker during the next iteration
+let COLLISION_MATRIX = build_matrix(WIDTH, HEIGHT);
+for w in 0..N_WORKERS {
     let x = worker(w).x;
     let y = worker(w).y;
 
-    collision_matrix[x][y] = 1;
+    COLLISION_MATRIX[x][y] = 1; // 1 means occupied - 0 means free
 }
 
-info(`collision matrix ${collision_matrix[34][33]}`);
-
-// version
-let version = 1;
-info(`version ${version}`);
-
-let occupied_positions = [];
-let number_of_occupied_positions = 0;
 
 fn build_matrix(width, height) {
     /*
-    It builds a matrix initializing it to 0
+        It builds a matrix and it initialize each element to 0
     */
 
     let matrix = [];
@@ -67,59 +32,70 @@ fn build_matrix(width, height) {
     return matrix;
 }
 
-fn find_neighbours_positions(current_position, map, width, height) {
-    let neighbours_positions = [];
 
-    let x = current_position[0];
-    let y = current_position[1];
+fn find_neighbours_positions(target_position, map, width, height) {
+    /*
+        It returns an array of positions that are adjacent (4-connectivity) to the target_position
+        It returns only the legal positions, i.e. positions that are inside the map
+    */
+    let neighbours_positions = []; // array to return
 
-    let target_x = 0;
-    let target_y = 0;
+    let target_x = target_position[0];
+    let target_y = target_position[1];
 
     // left
-    target_x = x - 1;
-    target_y = y;
+    let candidate_x = target_x - 1;
+    let candidate_y = target_y;
 
-    if target_x >= 0 && target_x < width && target_y >= 0 && target_y < height {
+    if candidate_x >= 0 && candidate_x < width && candidate_y >= 0 && candidate_y < height {
         // legal move
-        neighbours_positions.push([target_x, target_y])
+        neighbours_positions.push([candidate_x, candidate_y])
     }
 
     // top
-    target_x = x;
-    target_y = y + 1;
+    candidate_x = target_x;
+    candidate_y = target_y + 1;
 
-    if target_x >= 0 && target_x < width && target_y >= 0 && target_y < height {
+    if candidate_x >= 0 && candidate_x < width && candidate_y >= 0 && candidate_y < height {
         // legal move
-        neighbours_positions.push([target_x, target_y])
+        neighbours_positions.push([candidate_x, candidate_y])
     }
 
     // right
-    target_x = x + 1;
-    target_y = y;
+    candidate_x = target_x + 1;
+    candidate_y = target_y;
 
-    if target_x >= 0 && target_x < width && target_y >= 0 && target_y < height {
+    if candidate_x >= 0 && candidate_x < width && candidate_y >= 0 && candidate_y < height {
         // legal move
-        neighbours_positions.push([target_x, target_y])
+        neighbours_positions.push([candidate_x, candidate_y])
     }
 
     // bottom
-    target_x = x;
-    target_y = y - 1;
+    candidate_x = target_x;
+    candidate_y = target_y - 1;
 
-    if target_x >= 0 && target_x < width && target_y >= 0 && target_y < height {
+    if candidate_x >= 0 && candidate_x < width && candidate_y >= 0 && candidate_y < height {
         // legal move
-        neighbours_positions.push([target_x, target_y])
+        neighbours_positions.push([candidate_x, candidate_y])
     }
 
     return neighbours_positions;
 }
 
+
 fn find_team_color() {
+    /*
+        It returns the color associated to the our team
+    */
     return worker(0).color;
 }
 
+
 fn find_corner_position(width, height) {
+    /*
+        It returns the position [x, y] of our spawning corner
+        The computation is based on the workers, so you should call this function only once, in the beginning of the game
+    */
     let position = [0, 0];
     if worker(0).x < (width / 2) {
         // left side
@@ -148,6 +124,8 @@ fn find_corner_position(width, height) {
     return position;
 }
 
+
+/*
 fn find_closest_colorable_tiles_to_position(n, target_position, map, width, height, team_color) {
     /*
     It finds the first "n" closest colorable tiles to the target position (it only works for corner positions)
@@ -191,12 +169,12 @@ fn find_closest_colorable_tiles_to_position(n, target_position, map, width, heig
     
     return closest_colorable_tiles;
 }
+*/
 
 
 fn find_closest_colorable_tiles_to_position_equal_distance(target_position, map, width, height, team_color) {
     /*
-    It finds the first "n" closest colorable tiles to the target position (it only works for corner positions)
-    It finds all tiles at the same distance
+    It returns the closest colorable tiles to the target position. If some tiles has the same distance, it returns all of them.
 
     A colorable tile is a tile that is empty or colored with an enemy color
     */
@@ -213,7 +191,7 @@ fn find_closest_colorable_tiles_to_position_equal_distance(target_position, map,
     let target_x = target_position[0];
     let target_y = target_position[1];
     visited_matrix[target_x][target_y] = 1;
-    let found_level = -1;
+    let stop_level = -1;
 
     while len(queue) > 0 {
         level = level + 1;
@@ -224,11 +202,11 @@ fn find_closest_colorable_tiles_to_position_equal_distance(target_position, map,
         let y = current_position[1];
 
         if map[x][y] != team_color {
-            if found_level == -1 || found_level == current_level {
+            if stop_level == -1 || stop_level == current_level {
                 closest_colorable_tiles.push(current_position);
-                found_level = current_level;
+                stop_level = current_level;
             }
-            if current_level > found_level {
+            if current_level > stop_level {
                 return closest_colorable_tiles;
             }
         }
@@ -239,7 +217,7 @@ fn find_closest_colorable_tiles_to_position_equal_distance(target_position, map,
             let neighbour_x = neighbour_position[0];
             let neighbour_y = neighbour_position[1];
             if visited_matrix[neighbour_x][neighbour_y] == 0 {
-                // if not visited yet
+                // if it has not been visited yet
                 queue.push([neighbour_position, level]);
                 visited_matrix[neighbour_x][neighbour_y] = 1;
             }
@@ -260,13 +238,12 @@ fn dump_positions(positions) {
     }
 }
 
-fn move_to_position(worker, position, collision_matrix) {
-    // move towards the position if possible
+fn move_to_position(worker, target_position, collision_matrix) {
+    // Move towards the target_position if possible.
+    // Otherwise if there is an obstacle (a worker with the same color) it try to change direction otherwise it stops.
 
-    // just move to position for now
-
-    let target_x = position[0];
-    let target_y = position[1];
+    let target_x = target_position[0];
+    let target_y = target_position[1];
 
     let local_target_x = 0;
     let local_target_y = 0;
@@ -274,7 +251,7 @@ fn move_to_position(worker, position, collision_matrix) {
     // x
     let x_difference = worker.x - target_x;
     if x_difference < 0 {
-        // go left
+        // go right
         local_target_x = worker.x + 1;
         local_target_y = worker.y;
         if collision_matrix[local_target_x][local_target_y] == 0 {
@@ -285,7 +262,7 @@ fn move_to_position(worker, position, collision_matrix) {
         }
     }
     if x_difference > 0 {
-        // go right
+        // go left
         local_target_x = worker.x - 1;
         local_target_y = worker.y;
         if collision_matrix[local_target_x][local_target_y] == 0 {
@@ -295,7 +272,6 @@ fn move_to_position(worker, position, collision_matrix) {
             return collision_matrix;
         }
     }
-
 
     // y
     let y_difference = worker.y - target_y;
@@ -327,68 +303,31 @@ fn move_to_position(worker, position, collision_matrix) {
 
 }
 
+
 fn compute_distance(position_a, position_b) {
     let distance = abs(position_a[0] - position_b[0]) + abs(position_a[1] - position_b[1]);
 
     return distance;
 }
 
+
 // I should do this only once
-if memory.registered_corner_position != `` {
-    memory.corner_position = find_corner_position(width, height);
-    memory.registered_corner_position = ``;
+if "corner_position" in memory == false {
+    memory.corner_position = find_corner_position(WIDTH, HEIGHT);
 }
 let team_color = find_team_color();
 info(`corner position: ${memory.corner_position}`);
 info(`team color: ${team_color}`);
 
-let closest_colorable_tiles = find_closest_colorable_tiles_to_position(n_workers, memory.corner_position, map, width, height, team_color);
-// dump_positions(closest_colorable_tiles);
-
-if len(closest_colorable_tiles) < n_workers {
-    // add random positions
-    let remaining_positions_number = n_workers - len(closest_colorable_tiles);
-    for i in 0..remaining_positions_number {
-        let random_x = (rand() % width).abs();
-        let random_y = (rand() % height).abs();
-        closest_colorable_tiles.push([random_x, random_y]);
-    }
-}
-
-/*
-for w in 0..n_workers {
-    let min_distance = 1000;
-    let position_index = 1000;
-    let worker = worker(w);
-    for i in 0..len(closest_colorable_tiles) {
-
-        // I should not change the target once I chose the it.
-        let position = closest_colorable_tiles[i];
-
-        let distance = compute_distance(position, [worker.x, worker.y]);
-        if distance < min_distance {
-            min_distance = distance;
-            position_index = i;
-        }
-    }
-
-    collision_matrix = move_to_position(worker, closest_colorable_tiles[position_index], collision_matrix);
-    info(`${closest_colorable_tiles[position_index]} at ${worker.x} - ${worker.y}`);
-}
-*/
-
-// for each worker find the closest empty or enemy tile
-for w in 0..n_workers {
+// For each worker find the closest empty or enemy tile
+for w in 0..N_WORKERS {
     let worker = worker(w);
     let worker_position = [worker.x, worker.y];
-    let positions = find_closest_colorable_tiles_to_position_equal_distance(worker_position, map, width, height, team_color);
-    // chose a random position
-    // if positions is Null I have to chose random I think
-    let index = (rand() % len(positions)).abs();
-    collision_matrix = move_to_position(worker, positions[index], collision_matrix);
-    info(`${positions[0]} at ${worker.x} - ${worker.y}`);
+    let positions = find_closest_colorable_tiles_to_position_equal_distance(worker_position, map, WIDTH, HEIGHT, team_color);
+    if len(positions) > 0 {
+        // Select a random position from the proposed ones
+        let index = (rand() % len(positions)).abs();
+        COLLISION_MATRIX = move_to_position(worker, positions[index], COLLISION_MATRIX);
+        // info(`${worker.x} - ${worker.y} moves to ${positions[0]}`);
+    }
 }
-
-
-// dump corner color
-//info(`corner color: ${map[39][39]}`);
